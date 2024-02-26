c969484a455143066db3fdba0c45c5b894ffb4fd
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-02-26 17:01:49 -0300

    Add react-navigation dependencies and implement navigation in App.jsx

---
+++ src/screens/Profile/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        alingItems: 'center',
+        justifyContent: 'center'
+    },
+    navButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: 'lightgray',
+        color: 'black',
+        borderRadius: 5,
+    },
+});
+
+export default styles;
